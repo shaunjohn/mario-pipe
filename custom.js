9e5bc3d26e4501c598ba4717a4a307304f74668c
Shaun Johnson <Shaun@Shauns-MacBook-Air.local>
2015-06-24 08:55:00 -0400

    added comments to html + js

--- custom.js
+++ custom.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function(){ 
 
-	$("#pipe").click
+	$("#pipe").click // when the element with the id "pipe" is clicked..
 	(function(){
-		$("#mario").toggle({"marginBottom":"-20px"});
+		$("#mario").toggle({"marginBottom":"-20px"}); // ...vary the margin bottom of the element with the "mario" id from +/- 20px
 	});
-
-
 
 });
